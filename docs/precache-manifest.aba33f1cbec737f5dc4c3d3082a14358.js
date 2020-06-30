c57bbe8852588ee0d8b2d67d38ff395055c95992
KASHAN HAIDER <smkh.kashan@gmail.com>
2020-06-30 20:53:47 +0500

    done

---
+++ docs/precache-manifest.aba33f1cbec737f5dc4c3d3082a14358.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e23752005f9820f51b3e8b78dacca50",
+    "url": "/Would-You-Rather/index.html"
+  },
+  {
+    "revision": "1f885152d9b15b40a1eb",
+    "url": "/Would-You-Rather/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "38178d45663469b60295",
+    "url": "/Would-You-Rather/static/css/main.42c980d3.chunk.css"
+  },
+  {
+    "revision": "1f885152d9b15b40a1eb",
+    "url": "/Would-You-Rather/static/js/2.6e0e3304.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Would-You-Rather/static/js/2.6e0e3304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38178d45663469b60295",
+    "url": "/Would-You-Rather/static/js/main.b1aa45d4.chunk.js"
+  },
+  {
+    "revision": "b5b94c35de405cf73c05",
+    "url": "/Would-You-Rather/static/js/runtime-main.9c1621fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Would-You-Rather/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ae798b0b2f6aa3ce2e197ba4dfb681a.js"
+  "/Would-You-Rather/precache-manifest.aba33f1cbec737f5dc4c3d3082a14358.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Would-You-Rather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
