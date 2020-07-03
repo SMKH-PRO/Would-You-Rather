d0fb66363e601cd2e17751ae53983929cb9ba107
KASHAN HAIDER <smkh.kashan@gmail.com>
2020-07-03 18:33:23 +0500

    ghpages

---
+++ build/precache-manifest.98dc51de8bf55ede0d830276565ee2f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a6864cc1b40619037c8b3b7b96d03e",
+    "url": "/Would-You-Rather/index.html"
+  },
+  {
+    "revision": "465053db54ed7f5fc175",
+    "url": "/Would-You-Rather/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "021cd9db235dd4e4a829",
+    "url": "/Would-You-Rather/static/css/main.8839b119.chunk.css"
+  },
+  {
+    "revision": "465053db54ed7f5fc175",
+    "url": "/Would-You-Rather/static/js/2.d56a77cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Would-You-Rather/static/js/2.d56a77cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "021cd9db235dd4e4a829",
+    "url": "/Would-You-Rather/static/js/main.cb16d279.chunk.js"
+  },
+  {
+    "revision": "b5b94c35de405cf73c05",
+    "url": "/Would-You-Rather/static/js/runtime-main.9c1621fd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Would-You-Rather/static/media/logo.ee7cd8ed.svg"
+  }
+]);
